ebb54cf04731a1211ba0b577e39449343127eca4
shazzdev <81687930+shazzdev@users.noreply.github.com>
2024-01-11 20:56:46 +0530

    update C/hello.c

--- C/hello.c
+++ C/hello.c
@@ -2,5 +2,5 @@
 int main()          //create a function named main(code always starts inside main function)
 {                   //Your code must be inside these curly braces
   printf("Hello mom\n");//prints "Hello World". \n creates new line
-  return 0;
+  return 0;         //main function is int type. So it must return an integer. We made it return 0
 }
