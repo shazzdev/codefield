8af8ca636b64f02b0f6d32c0cdefbf077a5a03c8
shazzdev <81687930+shazzdev@users.noreply.github.com>
2024-01-11 20:52:49 +0530

    update C/hello.c

--- C/hello.c
+++ C/hello.c
@@ -1,5 +1,6 @@
 #include <stdio.h>  //include information about standard library
-int main(){         //include a function named main
-  printf("Hello mom");
+int main()          //include a function named main(code starts inside main function)
+{                   //statements of function must be inside curly braces
+  printf("Hello mom\n");//calls printf function from stdio.h to print Hello mom. \n means new line
   return 0;
 }
