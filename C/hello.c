393f1ceb2f6746e6c6adcac1707010ee2b10e264
shazzdev <81687930+shazzdev@users.noreply.github.com>
2024-01-11 20:57:50 +0530

    Delete the file at 'C/hello.c'

--- C/hello.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>  //add standard library
-int main()          //create a function named main(code always starts inside main function)
-{                   //Your code must be inside these curly braces
-  printf("Hello mom\n");//prints "Hello World". \n creates new line
-  return 0;         //main function is int type. So it must return an integer. We made it return 0
-}
